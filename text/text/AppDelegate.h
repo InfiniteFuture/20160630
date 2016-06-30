7b27c84c5166760c0fe7c156e4e17b0f3d50c4f0
Infinite <740195629@qq.com>
2016-06-30 16:14:26 -0700

    初始化项目

---
+++ text/text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by Infinite on 16/6/30.
+//  Copyright © 2016年 Infinite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text/text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by Infinite on 16/6/30.
+//  Copyright © 2016年 Infinite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
